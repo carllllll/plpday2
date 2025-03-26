3976cb9e8112b3e90e0f80d61d923d983080ea3c
carllllll <carlllmugo@gmail.com>
2025-03-26 15:20:49 +0300

    Add files via upload

---
+++ Week 2.py
@@ -0,0 +1,13 @@
+my_list=[]
+my_list.extend([10, 20, 30, 40])
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.sort()
+index_30= my_list.index(30)
+print("My Final list: ", my_list)
+print("Index of 30:", index_30)
+
+
