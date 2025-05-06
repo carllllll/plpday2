41ef8208ae486200b9e1dcbe8b47dafb0ed41adb
carllllll <carlllmugo@gmail.com>
2025-05-06 19:42:28 +0300

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,17 @@
+class superman:
+    def __init__(self,power):
+        self.power=power
+
+power=("super strength","flying","heat vision")
+print(power)
+
+
+class superheroes:
+    def __init__(self,move):
+        self.move=move
+
+flash=superheroes("super speed")
+print(flash.move)
+wonderwoman=superheroes("flight")
+print(wonderwoman.move) 
+    
